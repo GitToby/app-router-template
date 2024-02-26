dbc8e84e6b204c2cc68c0db98090327c556ea0ea
Toby Devlin <toby@tobydevlin.com>
2024-02-26 22:43:02 +1100

    missed server action

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,11 @@
+"use server"
 export type GreetingData = {
     name: string,
     greeting: string
 }
 
 export async function sayHello(name: string): Promise<GreetingData> {
-    console.log("doing response")
+    console.log("doing server response")
     // just do some string manipulation and echo the name.
     return {
         name: name,
